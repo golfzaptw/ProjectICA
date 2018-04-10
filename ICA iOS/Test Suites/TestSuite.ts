02fb197378030a30b3fc01b343251318cd2f7608
20ci <20ci@localhost>
2018-04-10 16:38:40 +0700

    Intitial Commit

---
+++ ICA iOS/Test Suites/TestSuite.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-04-10T16:20:27</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>161b9424-aa36-4381-b36e-bfef1634dea7</testSuiteGuid>
+   <testCaseLink>
+      <guid>547738cc-7e7b-440a-8442-21e67eb60842</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Simple</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>fb8ca577-ecd4-4c20-b055-934244236d8e</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
