cb3655d0d8053d3206679ddb02c5efe3a3d62a6a
Patawee <psgolf16@gmail.com>
2018-06-06 12:38:22 +0700

    TestSuite Register

---
+++ ICA Android/Test Suites/Release 0.0.1/Register.ts
@@ -0,0 +1,62 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Register</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-06-06T11:53:03</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>60</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>fb176de0-a47c-43aa-a20c-3a0beab9cd32</testSuiteGuid>
+   <testCaseLink>
+      <guid>bf6c6daf-2e90-4f91-afc6-b55a7e0934e0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Release 0.0.1/Authentication/Register/Register01</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c8d176e9-4092-48eb-8aa2-ab9f24506f46</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Release 0.0.1/Authentication/Register/Register02</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>06db2ab4-e042-40ec-963a-62da27db3909</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Release 0.0.1/Authentication/Register/Register03</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b2b67cb4-793f-4a7c-9d69-324061066193</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Release 0.0.1/Authentication/Register/Register04</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5065c771-ca25-45a5-aeb0-31829c7bc23e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Release 0.0.1/Authentication/Register/Register05</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>29d1eb0b-8719-4cf1-a242-2adf7754e837</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Release 0.0.1/Authentication/Register/Register06</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>48917afe-a008-403a-be16-e4de47949ad2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Release 0.0.1/Authentication/Register/Register07</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7d0bad16-7eb0-4840-93be-ee771426b4f1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Release 0.0.1/Authentication/Register/Register09</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
