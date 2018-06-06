9e099f0a9bb2657fb5a6ce0bfd3c0c64d2cc1639
Patawee <psgolf16@gmail.com>
2018-06-06 12:38:11 +0700

    TestSuite Login

---
+++ ICA Android/Test Suites/Release 0.0.1/Login.ts
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-06-06T12:21:21</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>60</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>32379c13-0442-41b4-85cc-9322084051cd</testSuiteGuid>
+   <testCaseLink>
+      <guid>3607c660-0cb7-4a88-ace1-660d652b5569</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Release 0.0.1/Authentication/Login/Login01</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>20569647-95a7-46d2-99c1-57b140379b3b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Release 0.0.1/Authentication/Login/Login02</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6c69db25-560d-4154-b2cd-e4e79c76047c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Release 0.0.1/Authentication/Login/Login03,04</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>00c21d03-5c82-4af1-a025-346b08d9bdc9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Release 0.0.1/Authentication/Login/Login05</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
